5179456bb6dfa5c197506ec08cb0f7a52b101805
namirinz <nathmcfc31115@gmail.com>
2020-04-14 23:30:30 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
+int mem[10000000];
+int fib(int n){
+    mem[1] = 1;
+    mem[2] = 1;
+    for (int i=3;i<=n;i++){
+        mem[i] = mem[i-1]+mem[i-2];
+    }
+    return mem[n];
+}
+
 int main(){
-    cout << "Hello World";
+    int n;
+    cin >> n;
+    cout << fib(n);
     return 0;
 }
